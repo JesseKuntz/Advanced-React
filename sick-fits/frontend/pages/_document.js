dc67905ae3aec89f5f8f300a3e721bfe2f5327b2
JesseKuntz <jessekuntz96@gmail.com>
2021-08-18 14:05:47 -0500

    feat: part 8

--- sick-fits/frontend/pages/_document.js
+++ sick-fits/frontend/pages/_document.js
@@ -3,7 +3,7 @@ import Document, { Html, Head, NextScript, Main } from 'next/document';
 export default class MyDocument extends Document {
   render() {
     return <Html lang="en-US">
-
+      <Head />
       <body>
         <Main />
         <NextScript />
